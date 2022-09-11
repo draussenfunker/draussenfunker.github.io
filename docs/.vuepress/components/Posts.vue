d8a126fe2b45459c54a9ca019d1f2026c29a9f0e
jz <jz@chaos.li>
2022-09-12 01:01:17 +0200

    fix page and sorting

--- docs/.vuepress/components/Posts.vue
+++ docs/.vuepress/components/Posts.vue
@@ -1,11 +1,13 @@
 <template>
     <div v-for="page in pages">
         <div class="page-detail">
-            <router-link :to="page.path"><h2>{{ page.title }}
-            <template v-if="page.frontmatter.features">
-                <Badge v-for="feature in page.frontmatter.features"  type="tip" :text="feature" vertical="top" />
-            </template>
-            </h2> </router-link>
+            <router-link :to="page.path">
+                <h2>{{ page.title }}
+                    <template v-if="page.frontmatter.features">
+                        <Badge v-for="feature in page.frontmatter.features"  type="tip" :text="feature" vertical="top" />
+                    </template>
+                </h2>
+            </router-link>
             <img v-if="page.frontmatter.image" class="article-image" :src="page.frontmatter.image"/>
             <p>{{ page.frontmatter.description }}</p>
             <p><router-link :to="page.path">Mehr Erfahren</router-link></p>
