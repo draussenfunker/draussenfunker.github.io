0efa73520b94e04229009a19cdbc5259b5333b8c
julius zeidler <julius.zeidler@sumcumo.com>
2023-01-13 00:53:41 +0100

    pipeline

---
+++ optimize.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ npx sharp-cli resize 800 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs  \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
