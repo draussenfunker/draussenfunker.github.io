f52ca09d2e7540dac4de413cb32461c532c92f70
Julius Zeidler <zeidlos@users.noreply.github.com>
2024-06-01 20:21:36 +0000

    testing

--- optimize.sh
+++ optimize.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
- npx sharp-cli resize 800 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs  \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
+echo "running image optimization"
+npx sharp-cli resize 800 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs  \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
