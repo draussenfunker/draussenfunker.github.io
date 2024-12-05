dbf49282bd449a32724aa0e3d22659a871a840ac
Julius Zeidler <julius.zeidler@sapiens.com>
2024-12-06 00:18:34 +0100

    ignore errors

--- optimize.sh
+++ optimize.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "running image optimization"
-npx --yes sharp-cli resize 1200 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
-
+npx --yes sharp-cli resize 1200 --withoutEnlargement true --optimise true --progressive true --withMetadata false --failOn none --input $(find ./docs \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
