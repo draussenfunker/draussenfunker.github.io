c585bcb13ecdf3c765ed592ff13763ff2fd586b3
Julius Zeidler <julius.zeidler@sapiens.com>
2024-12-05 23:23:40 +0100

    fix optimize job

--- optimize.sh
+++ optimize.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "running image optimization"
-npx sharp-cli resize 800 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs  \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
+npx --yes sharp-cli resize 1200 --withoutEnlargement true --optimise true --progressive true --withMetadata false --input $(find ./docs \( -name cache -prune \) -o -name '*' -type f -exec file {} \; | awk -F: '{ if ($2 ~/[Ii]mage|EPS/) print $1}') --output "{dir}/{base}"
+
